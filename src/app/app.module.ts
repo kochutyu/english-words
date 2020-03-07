347b9844ba0b22eda503a2eb0050e40bf065d37b
Yura Kochut <kochutyurawork@gmail.com>
2020-03-07 12:15:38 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// 1. firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { environment } from 'src/environments/environment';
+import { FormsModule }   from '@angular/forms';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { BtnComponent } from './components/btn/btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignInComponent,
+    BtnComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // 3. Initialize
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCG7jQNLgRXzVrFo3o_Pxl5AK5zkx_NZEk",
+    authDomain: "english-words-689bc.firebaseapp.com",
+    databaseURL: "https://english-words-689bc.firebaseio.com",
+    projectId: "english-words-689bc",
+    storageBucket: "english-words-689bc.appspot.com",
+    messagingSenderId: "81558376619",
+    appId: "1:81558376619:web:4e1599923d0eba11eea7ee",
+    measurementId: "G-9W6W7P92Y6"
+  }
 };
 
 /*

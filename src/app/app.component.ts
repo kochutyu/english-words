4aa0ab532496d63bef4707ff64f3ba6d5108d66e
Yura Kochut <kochutyurawork@gmail.com>
2020-03-12 02:02:43 +0200

    update (add structure app.c.ts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from './shared/services/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'english-words';
+  constructor(
+    public modal: ModalService
+  ){}
 }

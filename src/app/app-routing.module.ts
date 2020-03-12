c6232c57c07ae435083745fa9712d622621d7bae
Yura Kochut <kochutyurawork@gmail.com>
2020-03-12 02:01:15 +0200

    add (routing)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './components/sign-in/sign-in.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'login', component: HeaderComponent },
-  { path: 'sign-in', component: SignInComponent },
+  { path: 'home', component: HomeComponent },
+  // { path: 'login', component: HeaderComponent },
+  // { path: 'sign-in', component: SignInComponent },
   { path: '**', redirectTo: '/home' }
 ];
 

9917c81de21dcfed4b532f8d4e05715059f00eab
Yura Kochut <kochutyurawork@gmail.com>
2020-03-25 17:39:55 +0200

    update ( not-learned-words | functionality is done )

--- src/app/pages/not-learned-words/not-learned-words.component.ts
+++ src/app/pages/not-learned-words/not-learned-words.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { UsersService } from 'src/app/shared/services/users.service';
+import { CardService } from 'src/app/shared/services/card.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-not-learned-words',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotLearnedWordsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userS: UsersService,
+    private cardS: CardService,
+    private r: Renderer2,
+    private fs: AngularFirestore
+  ) { }
 
   ngOnInit(): void {
+    this.randomWord();
+  }
+
+  randomWord(): void {
+
+    // get user`s notLarnedWords
+    this.cardS.words = this.userS.user.notLarnedWords.map(item => JSON.parse(item));
+
+    if (this.userS.user.notLarnedWords.length > 0) {
+      this.cardS.randomWord();
+    } else {
+
+      // if haven`t words in notLarnedWords
+      delete this.cardS.word
+      delete this.cardS.transcription
+      delete this.cardS.translate
+    }
+
+  }
+
+  nextWord(): void {
+    if (this.userS.user.notLarnedWords.length > 0) {
+
+      // learned user word
+      const learnedWords: string[] = this.userS.user.learnedWords;
+      learnedWords.push(JSON.stringify(this.cardS.previousWord));
+
+      // delete user word
+      this.userS.user.notLarnedWords = this.userS.user.notLarnedWords.filter(item => item !== JSON.stringify(this.cardS.previousWord));
+      const notLarnedWords = this.userS.user.notLarnedWords;
+
+      // get new word
+      this.randomWord();
+
+      // get progress
+      this.r.setStyle(this.userS.progress.nativeElement, 'width', `${this.userS.getProgressLearnewWords()}%`);
+
+      // update database
+      this.fs.collection('users').doc(this.userS.user.id).update({
+        learnedWords,
+        notLarnedWords
+      });
+    }
+  }
+
+  didNotRemeberWord(): void {
+    this.randomWord()
   }
 
 }

033118f1e73724245ce83937a936c7a78261cc53
Yura Kochut <kochutyurawork@gmail.com>
2020-03-27 20:09:16 +0200

    update ( home | сleaning up extra code )

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FireService } from 'src/app/shared/services/fire.service';
-import { IWords } from 'src/app/shared/model/words';
-import { CardService } from 'src/app/shared/services/card.service';
-import { UsersService } from 'src/app/shared/services/users.service';
-import { Subscription } from 'rxjs';
-import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: 'app-home',
@@ -12,23 +6,9 @@ import { ModalService } from 'src/app/shared/services/modal.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  words: IWords[] = [];
-  subcsribe$: Subscription;
-  constructor(
-    public db: FireService,
-    private cardS: CardService,
-    public userS: UsersService,
-    public modal: ModalService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  change(): void {
-    if (sessionStorage.getItem('words')) {
-      const words: IWords[] = JSON.parse(sessionStorage.getItem('words'))
-      this.cardS.randomWord();
-    }
   }
 
 }
